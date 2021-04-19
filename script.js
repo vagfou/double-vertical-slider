b93e54d0f1b5fe14759cb884d28c278c098570c8
vagfou <foufoulas.evangelos@protonmail.com>
2021-04-19 20:11:52 +0300

    changeSlide coded

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const slider = document.querySelector('.slider-container')
+const sliderContainer = document.querySelector('.slider-container')
 const slideRight = document.querySelector('.right-slide')
 const slideLeft = document.querySelector('.left-slide')
-const upButtonn = document.querySelector('.up-button')
+const upButton = document.querySelector('.up-button')
 const downButton = document.querySelector('.down-button')
 const slidesLength = slideRight.querySelectorAll('div').length
 
@@ -9,6 +9,17 @@ let activeSlideIndex = 0
 
 slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
 
-upButtonn.addEventListener('click', () => {
-    
-})
+upButton.addEventListener('click', () => changeSlide('up'))
+downButton.addEventListener('click', () => changeSlide('down'))
+
+const changeSlide = (direction) => {
+    const sliderHeight = sliderContainer.clientHeight
+    if (direction === 'up') {
+        activeSlideIndex++
+        if (activeSlideIndex > slidesLength - 1) {
+            activeSlideIndex = 0
+        }
+    }
+
+    slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+}
