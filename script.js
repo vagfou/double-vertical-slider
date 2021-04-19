d068c0cb0490b7373c1ee9050bc6651e08c24133
vagfou <foufoulas.evangelos@protonmail.com>
2021-04-19 20:02:15 +0300

    JS variables declared

---
+++ script.js
@@ -0,0 +1,7 @@
+const slider = document.querySelector('.slider-container')
+const slideRight = document.querySelector('.right-slide')
+const slideLeft = document.querySelector('.left-slide')
+const upBtn = document.querySelector('.up-button')
+const downBtn = document.querySelector('.down-button')
+const slidesLength = slideRight.querySelectorAll('div').length
+
