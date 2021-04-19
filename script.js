a6ab80cefafbb34067f7351f0a65644610e1f4e6
vagfou <foufoulas.evangelos@protonmail.com>
2021-04-19 20:04:51 +0300

    upbutton function created

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const slider = document.querySelector('.slider-container')
 const slideRight = document.querySelector('.right-slide')
 const slideLeft = document.querySelector('.left-slide')
-const upBtn = document.querySelector('.up-button')
-const downBtn = document.querySelector('.down-button')
+const upButtonn = document.querySelector('.up-button')
+const downButton = document.querySelector('.down-button')
 const slidesLength = slideRight.querySelectorAll('div').length
 
+let activeSlideIndex = 0
+
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
+
+upButtonn.addEventListener('click', () => {
+    
+})
